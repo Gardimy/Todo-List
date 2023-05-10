764dac28e086b822a40b9b6739d401b3eedb54c3
Gardimy Charles <gardimycharles889@gmail.com>
2023-05-09 21:28:35 -0400

    fix linter and eslint error

--- webpack.config.js
+++ webpack.config.js
@@ -8,22 +8,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/,
-  //       use: ['style-loader', 'css-loader'],
-  //     },
-  //   ],
-  // },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     template: './src/index.html',
-  //   }),
-  // ],
-  // devServer: {
-  //   static: path.join(__dirname, 'dist'),
-  //   compress: true,
-  //   port: 8080,
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
 };
