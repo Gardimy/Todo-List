282dbfbe97f42f932b58ae344e89efa2c6d753ac
Gardimy Charles <gardimycharles889@gmail.com>
2023-05-10 23:15:01 -0400

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // src/index.js
 import populateTodoList from './modules/function.js';
-
+import './style.css';
 // Render the to-do list on page load
 window.addEventListener('load', populateTodoList);
